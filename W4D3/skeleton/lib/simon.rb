b01ebedd12ac49a5e161611a00c2e6e1309a3475
wliu <wliu0709@gmail.com>
2021-05-12 08:55:40 -0400

    homework w4d3

---
+++ W4D3/skeleton/lib/simon.rb
@@ -0,0 +1,57 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+      @sequence_length = 1
+      @seq = []
+      @game_over = false
+  end
+
+  def play
+     until @game_over
+    take_turn
+    end 
+    game_over_message
+    reset_game
+
+  end
+
+  def take_turn
+    show_sequence
+    require_sequence
+    if !@game_over
+    round_success_message
+    @sequence_length += 1
+    end 
+  end
+
+  def show_sequence
+    add_random_color
+  end
+
+  def require_sequence
+
+  end
+
+  def add_random_color
+    idx = rand(COLORS.length-1)
+    @seq << COLORS[idx]
+
+  end
+
+  def round_success_message
+
+  end
+
+  def game_over_message
+
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+end
