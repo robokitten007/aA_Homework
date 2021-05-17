95b0008b5cdbe70eca941cf7d9dc368f48d6b0cd
wliu <wliu0709@gmail.com>
2021-05-17 07:02:54 -0400

    lru

---
+++ W5D1/lru.rb
@@ -0,0 +1,26 @@
+lass LRUCache
+  def initialize(size)
+    @size = size
+    @cache = []
+  end
+
+  def count
+    @cache.count
+  end
+
+  def add(el)
+    if @cache.include?(el)
+      @cache.delete(el)
+      @cache << el
+    elsif count >= @size
+      @cache.shift
+      @cache << el
+    else
+      @cache << el
+    end
+  end
+
+  def show
+    p @cache
+  end
+end
