b4ce9bc3761360dbcb4cc39218a3345e50a97ab4
wliu <wliu0709@gmail.com>
2021-06-10 08:57:45 -0400

    w8d4

---
+++ w8d4/callback.js
@@ -0,0 +1,21 @@
+
+
+window.setTimeout(function(){
+    alert('HAMMERTIME');
+}, 5000);
+
+
+
+window.setTimeout(function hammerTime(time){
+    alert(`{$time} is hammertime!`);
+
+}, time);
+
+
+let readline = require('readline');
+let reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
